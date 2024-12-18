7e390ab7cf77dd072e2344339ca636388f4ad85a
Michael O'Brien <mpobrien005@gmail.com>
2024-12-17 20:56:13 -0500

    first commit

---
+++ db.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+	"time"
+
+	// "fmt"
+	_ "github.com/mattn/go-sqlite3"
+	// "log"
+	// "os"
+)
+
+type DB struct {
+	*sql.DB
+}
+
+const (
+	statusPending     int = 0
+	statusInProgress  int = 1
+	statusDoneSuccess int = 2
+	statusDoneFailed  int = 3
+)
+
+type Job struct {
+	ID         int    `db:"id"`
+	Command    string `db:"command"`
+	PID        int    `db:"pid"`
+	Status     int    `db:"status"`
+	CreatedAt  int64  `db:"created_at"`
+	StartedAt  int64  `db:"started_at"`
+	FinishedAt int64  `db:"finished_at"`
+}
+
+func (job Job) String() string {
+	sb := strings.Builder{}
+	sb.WriteString(fmt.Sprintf("%d: ", job.ID))
+	switch job.Status {
+	case statusPending:
+		sb.WriteString("[ ] ")
+	case statusInProgress:
+		sb.WriteString("[-] ")
+	case statusDoneSuccess:
+		sb.WriteString("[x] ")
+	case statusDoneFailed:
+		sb.WriteString("[!] ")
+	}
+	sb.WriteString(job.Command)
+	if job.PID > 0 {
+		sb.WriteString(fmt.Sprintf(" [%d]", job.PID))
+	}
+	if job.StartedAt != 0 {
+		if job.FinishedAt != 0 {
+			elapsed := time.UnixMilli(job.FinishedAt).Sub(time.UnixMilli(job.StartedAt))
+			sb.WriteString(fmt.Sprintf(" %s", elapsed))
+		} else {
+			elapsed := time.Since(time.UnixMilli(job.StartedAt))
+			sb.WriteString(fmt.Sprintf(" %s", elapsed))
+		}
+	}
+
+	return sb.String()
+}
+
+func (db *DB) SetJobPID(jobID int64, pid int64) error {
+	_, err := db.Exec("UPDATE jobs SET pid=? WHERE id=?", pid, jobID)
+	return err
+}
+
+func (db *DB) SetJobStatus(jobID int64, status int64) error {
+	_, err := db.Exec("UPDATE jobs SET status=?, finished_at=? WHERE id=?", status, time.Now().UnixMilli(), jobID)
+	return err
+}
+
+func (db *DB) TakeNextJob() (*Job, error) {
+	var job Job
+	if err := db.QueryRow(`
+	WITH selected_job AS (
+		SELECT * FROM jobs
+		WHERE status = 0
+		ORDER BY id ASC
+		LIMIT 1
+	)
+	UPDATE jobs SET status = 1, started_at=?
+	WHERE id = (SELECT id FROM selected_job)
+	RETURNING id, command, pid, status, created_at, started_at, finished_at;
+	`,
+		time.Now().UnixMilli(),
+	).
+		Scan(
+			&job.ID,
+			&job.Command,
+			&job.PID,
+			&job.Status,
+			&job.CreatedAt,
+			&job.StartedAt,
+			&job.FinishedAt,
+		); err != nil {
+		return nil, err
+	}
+	return &job, nil
+}
+
+// Deletes job with given ID. Returns true if the job existed.
+func (db *DB) DeleteJob(id int64) (bool, error) {
+	result, err := db.Exec(`DELETE FROM jobs WHERE id = ?`, id)
+	if err != nil {
+		return false, err
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return false, err
+	}
+	return rows > 0, nil
+}
+
+func (db *DB) ListJobs() ([]Job, error) {
+	rows, err := db.Query(`SELECT id, command, pid, status, created_at, started_at, finished_at FROM JOBS`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var jobs []Job
+
+	for rows.Next() {
+		var job Job
+		if err := rows.Scan(
+			&job.ID,
+			&job.Command,
+			&job.PID,
+			&job.Status,
+			&job.CreatedAt,
+			&job.StartedAt,
+			&job.FinishedAt,
+		); err != nil {
+			return jobs, err
+		}
+		jobs = append(jobs, job)
+	}
+	return jobs, nil
+}
+
+func (db *DB) AddJob(command string) (int64, error) {
+	result, err := db.Exec(`
+	BEGIN TRANSACTION;
+	INSERT INTO jobs (command, status, created_at, started_at, finished_at)  values (?,?,?,?,?);
+	COMMIT TRANSACTION;
+	`, command, statusPending, time.Now().UnixMilli(), 0, 0)
+	if err != nil {
+		return 0, err
+	}
+	return result.LastInsertId()
+}
+
+func Open(filename string) (*DB, error) {
+	db, err := sql.Open("sqlite3", filename)
+	if err != nil {
+		return nil, err
+	}
+
+	sqlStmt := `
+	BEGIN TRANSACTION;
+	create table if not exists jobs 
+	(
+		id integer not null primary key,
+		command text not null,
+		pid integer default 0,
+		status integer default 0,
+		created_at int default 0,
+		started_at int default 0,
+		finished_at int default 0
+	);
+	COMMIT TRANSACTION;
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DB{
+		DB: db,
+	}, nil
+}
---
+++ main.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+)
+
+const chimeDBPathEnvKey = "CHIME_DB_PATH"
+
+const (
+	runCommandName    = "run"
+	listCommandName   = "list"
+	addCommandName    = "add"
+	removeCommandName = "remove"
+)
+
+type globalArgs struct {
+	dbPath string
+}
+
+type run struct {
+	globalArgs
+	jobID int
+}
+type list struct {
+	globalArgs
+}
+type add struct {
+	globalArgs
+	commandToRun string
+}
+type remove struct {
+	globalArgs
+	id int
+}
+
+func main() {
+	var dbPath string
+	flag.StringVar(&dbPath, "dbpath", "", "path to DB file")
+	flag.Parse()
+
+	if len(dbPath) == 0 {
+		var ok bool
+		if dbPath, ok = os.LookupEnv(chimeDBPathEnvKey); !ok {
+			homedir, err := os.UserHomeDir()
+			if err != nil {
+				log.Fatalf("failed to find home dir: %s", err)
+			}
+
+			dbPath = filepath.Join(homedir, ".chime.db")
+		}
+	}
+
+	cmd, err := parseSubcommand(
+		globalArgs{
+			dbPath: dbPath,
+		},
+		flag.Args(),
+	)
+	if err != nil {
+		log.Printf("%s", err)
+		os.Exit(1)
+	}
+
+	if err := cmd.Run(); err != nil {
+		log.Printf("%s", err)
+		os.Exit(1)
+	}
+}
+
+type subcommand interface {
+	Run() error
+}
+
+func (r run) Run() error {
+	db, err := Open(r.globalArgs.dbPath)
+	if err != nil {
+		return fmt.Errorf("failed to open db: %w", err)
+	}
+	defer db.Close()
+
+	nextJob, err := db.TakeNextJob()
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("sh", "-c", nextJob.Command)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	runJobErr := func() error {
+		if err := cmd.Start(); err != nil {
+			return err
+		}
+		if err := db.SetJobPID(int64(nextJob.ID), int64(cmd.Process.Pid)); err != nil {
+			log.Printf("failed to set job pid: %s", err)
+		}
+
+		if err := cmd.Wait(); err != nil {
+			return err
+		}
+		return nil
+	}()
+
+	if runJobErr != nil {
+		if err := db.SetJobStatus(int64(nextJob.ID), int64(statusDoneFailed)); err != nil {
+			return fmt.Errorf("failed to set job status to failed (%s) for job error: %s", err, runJobErr)
+		}
+	} else {
+		if err := db.SetJobStatus(int64(nextJob.ID), int64(statusDoneSuccess)); err != nil {
+			return fmt.Errorf("failed to set job status to success for job error: %s", runJobErr)
+		}
+	}
+
+	return runJobErr
+}
+
+func (cmd list) Run() error {
+	db, err := Open(cmd.globalArgs.dbPath)
+	if err != nil {
+		return fmt.Errorf("failed to open db: %w", err)
+	}
+	defer db.Close()
+
+	jobs, err := db.ListJobs()
+	if err != nil {
+		return fmt.Errorf("failed to list jobs: %w", err)
+	}
+	for _, job := range jobs {
+		// TODO tabwriter formatted output
+		fmt.Println(job.String())
+	}
+	return err
+}
+
+func (cmd remove) Run() error {
+	db, err := Open(cmd.globalArgs.dbPath)
+	if err != nil {
+		return fmt.Errorf("failed to open db: %w", err)
+	}
+	defer db.Close()
+
+	_, err = db.DeleteJob(int64(cmd.id))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (cmd add) Run() error {
+	db, err := Open(cmd.globalArgs.dbPath)
+	if err != nil {
+		return fmt.Errorf("failed to open db: %w", err)
+	}
+	defer db.Close()
+
+	jobID, err := db.AddJob(cmd.commandToRun)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("added job #%d", jobID)
+	return nil
+}
+
+func parseSubcommand(globals globalArgs, args []string) (subcommand, error) {
+	if len(args) == 0 {
+		return nil, fmt.Errorf("no args specified")
+	}
+	cmd, args := args[0], args[1:]
+
+	switch cmd {
+	case runCommandName:
+		var jobID int
+		var err error
+		if len(args) == 1 {
+			if jobID, err = strconv.Atoi(args[0]); err != nil {
+				return nil, fmt.Errorf("param required: command to run")
+			}
+		}
+		return run{globalArgs: globals, jobID: jobID}, nil
+	case listCommandName:
+		return list{globalArgs: globals}, nil
+	case addCommandName:
+		if len(args) != 1 {
+			return nil, fmt.Errorf("param required: command to run")
+		}
+		return add{globalArgs: globals, commandToRun: args[0]}, nil
+	case removeCommandName:
+		if len(args) != 1 {
+			return nil, fmt.Errorf("param required: job ID to remove")
+		}
+		jobID, err := strconv.Atoi(args[0])
+		if err != nil {
+			return nil, fmt.Errorf("invalid job ID: '%s'", args[0])
+		}
+		return remove{
+			globalArgs: globals,
+			id:         jobID,
+		}, nil
+	}
+	return nil, fmt.Errorf("unknown command: '%s'", cmd)
+}
